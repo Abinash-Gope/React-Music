c0689e8b274360e069ef029ab8fa5412ab9cd8e9
Abinash Gope <abinashg797@gmail.com>
2026-08-11 22:37:36 +0530

    feat: add initial project setup with React, Vite, and Tailwind CSS
    - Added SVG assets for React and Vite.
    - Created Navbar component.
    - Set up main layout with Outlet for routing.
    - Implemented main entry point for the application.
    - Added pages: Home, Login, Register, ArtistDashboard, and Favrouite.
    - Configured React Router for navigation.
    - Integrated Tailwind CSS into the project.

---
+++ src/router/router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+import Favrouite from "../pages/Favrouite";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ArtistDashboard from "../pages/ArtistDashboard";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        Element: <MainLayout />,
+        children:[
+            {
+                index:true,
+                element: <Home />
+            },
+            {
+                path:"favrouite",
+                element: <Favrouite />
+            },
+            {
+                path:"login",
+                element: <Login />
+            },
+            {
+                path:"register",
+                element: <Register />
+            },
+            {
+                path:"artist-dashboard",
+                element: <ArtistDashboard />
+            },
+        ]
+    }
+])
+
+export default router;
